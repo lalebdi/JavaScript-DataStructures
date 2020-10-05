393c5854fd23f004b098ae8d7f777bb7df85d56a
lalebdi <loai.alebdi@gmail.com>
2020-10-05 13:01:31 -0400

    init

---
+++ binary_search_tree.js
@@ -0,0 +1,5 @@
+// each node has a max of 2 child nodes
+// the left child node is less than the parent node. The right child node is more than the parent node. 
+// Because of the principle of a binary search the average operation is going to skip about half the tree. So, each, seach, add or deletion takes O(log(n)). 
+
+// here I'm going to use classes to create a binary search tree. 
