7baee1cca2ff57ae94ae6ff70c18a33bc298514e
lalebdi <loai.alebdi@gmail.com>
2020-10-06 20:38:51 -0400

    done theory

--- linkedLists.js
+++ linkedLists.js
@@ -3,3 +3,5 @@
 //  1- the element itself.
 // 2- reference to the next node. 
 
+// Every linked List will have a head (the first node) and will have a size(the amount of nodes). the last node will point to null. 
+
