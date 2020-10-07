091f9124bf5d3c41666e68a7288640de8dbbd3b6
lalebdi <loai.alebdi@gmail.com>
2020-10-06 20:01:12 -0400

    done with the theory

---
+++ hashTables.js
@@ -0,0 +1,8 @@
+// is used to implment associative arrays or mappings of key-value pairs.
+// they are common to implment a common map data structure or objects. 
+// they are commonly used because they are efficient. The average time complexity is O(1) and the worst case is O(n). 
+// it takes a key input and runs it in a hash function. 
+//  a hash function maps strings to numbers. The numbers correspond to indexes of an array. It should map different strings to different numbers. 
+// if two strings get hashed to the same number, a collision occures. One way of handling a collsions is by storing both values in the same number, then iterated through that number to get the value from the number. this could be slow. 
+// Hash tables are built in JavaScript, which are used to implement objects. 
+
