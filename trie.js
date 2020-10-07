eaa3106c5e9f6172244d033e3d6659378b40eee3
lalebdi <loai.alebdi@gmail.com>
2020-10-06 21:39:45 -0400

    done

---
+++ trie.js
@@ -0,0 +1,3 @@
+// a prefix tree used to store associative data structures.
+// A trie stores data in steps. Each step is anode in the trie.
+// think of auto-correct, where the letters are stored in nodes and a chain of nodes will make up a word. 
